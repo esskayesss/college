108b87d5cf6dc4b4cdb14771682e214acce8db39
esskayesss <imesskayesss@gmail.com>
2022-03-12 11:20:57 +0530

    Updating 10 to find the key

--- intro-to-computing-lab/assn-1/10_fibonacci.c
+++ intro-to-computing-lab/assn-1/10_fibonacci.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    int n, t1 = 0, t2 = 1;
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
-    printf("%d\n%d\n", t1, t2);
-    for(int i = 2; i < n; i++){
-        int sum = t1 + t2;
-        printf("%d\n", sum);
-        t1 = t2;
-        t2 = sum;
+    int key, t1 = 0, t2 = 1, term = 0;
+    printf("Enter the key to search for in the fibonacci pattern: ");
+    scanf("%d", &key);
+    if(key < 0){
+        printf("%d is not present in the fibonacci series.\n", key);
+        return 0;
     }
+    while(term < key){
+        term = t1 + t2;
+        t1 = t2;
+        t2 = term;
+    }
+    if(term == key) printf("%d is found in the fibonacci series.\n", key);
+    else printf("%d is not in the fibonacci series.\n", key);
 }
